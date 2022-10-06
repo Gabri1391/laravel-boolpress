70f2268db28a383d1772188571fd80e0d2982260
Gabri1391 <chiarellogabriele@gmail.com>
2022-10-06 14:09:08 +0200

    npm run dev + .env edit

--- public/js/front.js
+++ public/js/front.js
@@ -54844,7 +54844,7 @@ var routes = new vue_router__WEBPACK_IMPORTED_MODULE_1__["default"]({
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! C:\Users\gioch\OneDrive\Desktop\laravel-auth-master\resources\js\front.js */"./resources/js/front.js");
+module.exports = __webpack_require__(/*! C:\Users\gioch\OneDrive\Desktop\laravel-boolpress\resources\js\front.js */"./resources/js/front.js");
 
 
 /***/ })
